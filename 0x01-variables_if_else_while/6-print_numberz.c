62dd191cc83dec1b83f54b2403f79d8a42666e5a
patrick <pchipungu50@gmail.com>
2023-03-17 11:18:49 -0700

    taskfix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,10 @@
 
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-printf("\n");
-return (0);
+	int num;
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
 }
